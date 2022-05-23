7411fcead0f4e14fcd002a8c0f142aa8c7403d0b
Felipe Navas <felipenavas.itec@gmail.com>
2022-05-23 20:41:24 -0300

    Adding bcrypt, jsonwebtoken, password encription, user model and token generator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,84 @@
 const { response } = require('express')
-const { validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+const { generateToken } = require('../helpers/jwt')
 
-const createUser = (req, res = response) => {
-  const { name, email, password } = req.body
-
-  res.status(201).json({
-    ok: true,
-    msg: 'registered',
-    name,
-    email,
-    password,
-  })
-}
-
-const loginUser = (req, res = response) => {
+const createUser = async (req, res = response) => {
   const { email, password } = req.body
 
-  res.json({
-    ok: true,
-    msg: 'login',
-    email,
-    password,
-  })
+  try {
+    let user = await User.findOne({ email })
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'User already exists',
+      })
+    }
+
+    user = new User(req.body)
+
+    const salt = bcrypt.genSaltSync()
+    user.password = bcrypt.hashSync(password, salt)
+
+    await user.save()
+
+    const token = await generateToken(user._id, user.name)
+
+    res.status(201).json({
+      ok: true,
+      msg: 'User created',
+      uid: user._id,
+      name: user.name,
+      token,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Error, try again or contact the administrator',
+    })
+  }
+}
+
+const loginUser = async (req, res = response) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'User not found',
+      })
+    }
+
+    const validPassword = bcrypt.compareSync(password, user.password)
+
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Invalid password',
+      })
+    }
+
+    const token = await generateToken(user._id, user.name)
+
+    res.json({
+      ok: true,
+      msg: 'login',
+      uid: user._id,
+      name: user.name,
+      token,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Error, try again or contact the administrator',
+    })
+  }
 }
 
 const revalidateToken = (req, res = response) => {
